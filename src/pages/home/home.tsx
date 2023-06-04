55957fd31e0529343375b8186bcf9e158b95a81a
zhaoyu-huawei <1069080359@qq.com>
2023-06-04 18:30:42 +0800

    政区 tree select 数据组装

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -4,17 +4,25 @@ import StepsDesc from './steps-desc';
 import SimpleDesc from './simple-desc';
 import PregnancyKnowledge from './pregnancy-knowledge';
 import './style.less';
+import CitySelect from '@/components/city-select';
 
 const Home = () => {
   return (
-    <Watermark
-      content={['我老婆最美', '我老婆最棒', '肤白貌美大长腿', '就是我老婆']}
-    >
-      <SimpleDesc />
-      <StepsDesc />
-      <PregnancyKnowledge />
-      <FloatButton type="primary" style={{ right: 24 }} tooltip={<QRCode />} />
-    </Watermark>
+    <>
+      <CitySelect />
+      <Watermark
+        content={['我老婆最美', '我老婆最棒', '肤白貌美大长腿', '就是我老婆']}
+      >
+        <SimpleDesc />
+        <StepsDesc />
+        <PregnancyKnowledge />
+        <FloatButton
+          type="primary"
+          style={{ right: 24 }}
+          tooltip={<QRCode />}
+        />
+      </Watermark>
+    </>
   );
 };
 export default Home;
