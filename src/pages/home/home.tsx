05de118cbbba4dd250c6dea6e4b1898d8dabba85
zhaoyu-huawei <1069080359@qq.com>
2023-06-04 19:05:02 +0800

    delet code

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -4,25 +4,17 @@ import StepsDesc from './steps-desc';
 import SimpleDesc from './simple-desc';
 import PregnancyKnowledge from './pregnancy-knowledge';
 import './style.less';
-import CitySelect from '@/components/city-select';
 
 const Home = () => {
   return (
-    <>
-      <CitySelect />
-      <Watermark
-        content={['我老婆最美', '我老婆最棒', '肤白貌美大长腿', '就是我老婆']}
-      >
-        <SimpleDesc />
-        <StepsDesc />
-        <PregnancyKnowledge />
-        <FloatButton
-          type="primary"
-          style={{ right: 24 }}
-          tooltip={<QRCode />}
-        />
-      </Watermark>
-    </>
+    <Watermark
+      content={['我老婆最美', '我老婆最棒', '肤白貌美大长腿', '就是我老婆']}
+    >
+      <SimpleDesc />
+      <StepsDesc />
+      <PregnancyKnowledge />
+      <FloatButton type="primary" style={{ right: 24 }} tooltip={<QRCode />} />
+    </Watermark>
   );
 };
 export default Home;
